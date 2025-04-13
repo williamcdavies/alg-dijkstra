be30b57f24dc049b5bba592b9c0664e71fcc8d49
William Chuter-Davies <152814048+williamcdavies@users.noreply.github.com>
2025-04-13 15:28:56 -0700

    update whitespace

--- directed_edge.h
+++ directed_edge.h
@@ -2,17 +2,18 @@
 #define DIRECTED_EDGE_H
 
 struct directed_edge
-{   public:
-        unsigned int p, q, d;
-        directed_edge() = default;
-        directed_edge(unsigned int p, unsigned int q, unsigned int d) : p(q), q(q), d(d) {}
-        directed_edge(const directed_edge& other) : p(other.p), q(other.q), d(other.d) {}
-        directed_edge& operator =(const directed_edge& rhs)
-        {   p = rhs.p;
-            q = rhs.q;
-            d = rhs.d;
-            return *this;
-        }
+{	public:
+		unsigned int p, q, d;
+		
+		directed_edge() = default;
+		directed_edge(unsigned int p, unsigned int q, unsigned int d) : p(q), q(q), d(d) {}
+		directed_edge(const directed_edge& other) : p(other.p), q(other.q), d(other.d) {}
+		directed_edge& operator =(const directed_edge& rhs)
+		{   p = rhs.p;
+			q = rhs.q;
+			d = rhs.d;
+			return *this;
+		}
 };
 
 #endif // DIRECTED_EDGE_H
