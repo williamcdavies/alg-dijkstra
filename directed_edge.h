f8697c63763ed3ab680f74c4c2b022127bcbf4ec
William Chuter-Davies <152814048+williamcdavies@users.noreply.github.com>
2025-04-15 11:55:08 -0700

    fix -Wunused-parameter warning

--- directed_edge.h
+++ directed_edge.h
@@ -6,7 +6,7 @@ struct directed_edge
 		unsigned int p, q, d;
 		
 		directed_edge() = default;
-		directed_edge(unsigned int p, unsigned int q, unsigned int d) : p(q), q(q), d(d) {}
+		directed_edge(unsigned int p, unsigned int q, unsigned int d) : p(p), q(q), d(d) {}
 		directed_edge(const directed_edge& other) : p(other.p), q(other.q), d(other.d) {}
 		directed_edge& operator =(const directed_edge& rhs)
 		{   p = rhs.p;
